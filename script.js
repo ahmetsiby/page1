312822d91674069d01eb89a6517730c28ad7e696
sibyahmet <siby@Mac-mini-de-sibyahmet.local>
2021-08-23 05:01:36 +0200

    cinq

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function toggleMenu() {
     const navbar = document.querySelector('.navbar');
     const burger = document.querySelector('.burger');
+    const main = document.querySelector('.main-content');
+    
+                
+                
     burger.addEventListener('click', (e) => {
         navbar.classList.toggle('show-nav');
+        /* permet de faire disparaitre ma barre de navigation et de remettre */
+        if(getComputedStyle(main).display !="none"){
+            main.style.display = "none";
+        } else {
+            main.style.display = "grid";
+        }
     });
+   
 }
 toggleMenu();
